50bf3d227a4c9f50c4f0b3ab81f65389279e8e1c
nhasey <nhasey@emich.edu>
2024-02-12 16:34:02 -0500

    added a message from first programmer

--- src/soloGithub/HelloWorld.java
+++ src/soloGithub/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Howdy github project");
-		System.out.println("alternate second print statment from alternate programmer");
+		System.out.println("alternate second print statment from alternate programmer + first programmer");
 	}
 
 }
